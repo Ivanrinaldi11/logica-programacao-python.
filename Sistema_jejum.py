1b57107e11d8c6d1a94969514fbb7401257691a4
Ivanrinaldi11 <ivanilsonvieira980@gmail.com>
2026-01-14 17:01:54 -0300

    Create Sistema_jejum.py

---
+++ Sistema_jejum.py
@@ -0,0 +1,8 @@
+
+# Início do programa
+resposta = input("Você está em jejum? (sim ou não): ")
+
+if resposta.lower() == "sim":
+    print("Entrada permitida. Você pode realizar a coleta de sangue.")
+else:
+    print("Entrada não permitida. Retorne quando estiver em jejum.")
